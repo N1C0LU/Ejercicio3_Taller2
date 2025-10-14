3a095d20afdccd0bb5647eff3e45602b398ac37f
N1COLU <nicolas.castano@uao.edu.co>
2025-10-14 10:56:09 -0500

    Se creo la clase padre CuentaBanco

--- src/main/java/Modelo/CuentaBanco.java
+++ src/main/java/Modelo/CuentaBanco.java
@@ -9,5 +9,45 @@ package Modelo;
  * @author Nicolas Castaño
  */
 public class CuentaBanco {
+    int numeroCuenta;
+    String nombreDueño;
+    int dia;
+    int mes;
+    int año;
+    double saldo;
     
+    public CuentaBanco (int numeroCuenta, String nombreDueño, int dia, int mes, int año){
+        this.numeroCuenta = numeroCuenta;
+        this.nombreDueño = nombreDueño;
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+        this.saldo = saldo;
+        }
+    
+    public String DatosCuenta (){
+        return "Numero de cuenta" +numeroCuenta+ "Nombre del propietario"+nombreDueño+ "Fecha de apertura"+dia+"/"+mes+"/"+año;    
+    }
+    
+    public void Consignar(double valor){
+        saldo += valor;
+    }
+    
+    public void retirar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente. No se puede retirar.");
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return DatosCuenta();
+    }
 }
+    
+   
+              
+     
+
