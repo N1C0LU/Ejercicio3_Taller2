723f2dbb0a760ab98a758683ea7f9063eeaadd82
N1COLU <nicolas.castano@uao.edu.co>
2025-10-14 10:15:23 -0500

    Se crearon las clases para el ejercicio

--- src/main/java/Modelo/Ahorros.java
+++ src/main/java/Modelo/Ahorros.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package Vista;
+package Modelo;
 
 /**
  *
  * @author Nicolas Castaño
  */
-public class main {
-
-    public static void main(String[] args) {
-        
-    }
+public class Ahorros {
+    
 }
