b09e986fb91a83dd0d9584764f316407adf6b5e1
N1COLU <nicolas.castano@uao.edu.co>
2025-10-14 18:04:01 -0500

    Se hizo la clase Corriente completa

--- src/main/java/Modelo/Corriente.java
+++ src/main/java/Modelo/Corriente.java
@@ -4,16 +4,39 @@
  */
 package Modelo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Nicolas Castaño
  */
 public class Corriente extends CuentaBanco {
-    double cupoSobregiro;
+    double cupoSobregiro = -100000;
     
     public Corriente (int numeroCuenta, String nombreDueño, int dia, int mes, int año, double saldo, double cupoSobregiro){
         super(numeroCuenta, nombreDueño, dia, mes, año, saldo); 
         this.cupoSobregiro = cupoSobregiro;
     }
     
+    @Override
+    public void retirar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else if (saldo - valor >= cupoSobregiro) {
+            saldo -= valor;
+            JOptionPane.showMessageDialog(null,"Se ha realizado su retiro con sobregiro. Saldo actual: $" + saldo);
+            } else {
+                JOptionPane.showMessageDialog(null, "No tienes más cupo de sobregiro");
+        } 
+    }
+    
+    public double getCupoSobregiro() {
+        return cupoSobregiro;
+    }
+
+    @Override
+    public String DatosCuenta() {
+        return super.DatosCuenta() + "  Cupo de sobregiro: $" + cupoSobregiro;
+        }
 }
+
